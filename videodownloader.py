2740f531249095e378384f4ca6f69081a8b6f571
Matheus Aguiar <matheustxaguiar@gmail.com>
2021-05-02 13:09:08 -0300

    Adding loop

--- videodownloader.py
+++ videodownloader.py
@@ -2,13 +2,15 @@ import pytube
 
 
 def main():
-    url = input("Insira a url do Vídeo: ")
-    yt = pytube.YouTube(url)
-    print(f"Titulo do Vídeo: {yt.title}")
-    stream = yt.streams.get_highest_resolution()
-    print("BAIXANDO VIDEO...")
-    stream.download()
-    print("Vídeo baixado, confira na sua pasta.")
+    while True:
+        url = input("Url do Vídeo: ")
+        yt = pytube.YouTube(url)
+        print(f"Titulo do Vídeo: {yt.title}")
+        stream = yt.streams.get_highest_resolution()
+        print("BAIXANDO VIDEO...")
+        stream.download()
+        print("Vídeo baixado, confira na sua pasta.")
+
 
 if __name__ == "__main__":
     main()
